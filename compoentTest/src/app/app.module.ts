a25e97779da6343e690010672459cc40ba862b1c
SherlinKuo <sherlin@checkone.com.tw>
2022-04-27 14:27:45 +0800

    ng-content

--- compoentTest/src/app/app.module.ts
+++ compoentTest/src/app/app.module.ts
@@ -6,13 +6,15 @@ import { AppComponent } from './app.component';
 import { HomeComponent } from './ch03/home/home.component';
 import { ChildComponent } from './ch03/child/child.component';
 import { ChildTwoWayComponent } from './ch03/child-two-way/child-two-way.component';
+import { ChildContentComponent } from './ch03/child-content/child-content.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
     ChildComponent,
-    ChildTwoWayComponent
+    ChildTwoWayComponent,
+    ChildContentComponent
   ],
   imports: [
     BrowserModule,
