5fbfb8ff0eb53d355e70cde8ab4db4d11b0c810e
SherlinKuo <sherlin@checkone.com.tw>
2022-04-28 11:37:13 +0800

    ngDoCheck

--- compoentTest/src/app/ch03/hook/hookhome/hookhome.component.ts
+++ compoentTest/src/app/ch03/hook/hookhome/hookhome.component.ts
@@ -1,14 +1,23 @@
-import { Component, OnChanges, OnInit, SimpleChanges } from '@angular/core';
+import { Component, DoCheck, OnChanges, OnInit, SimpleChanges } from '@angular/core';
 
 @Component({
   selector: 'app-hookhome',
   templateUrl: './hookhome.component.html',
   styleUrls: ['./hookhome.component.scss']
 })
-export class HookhomeComponent implements OnInit {
+export class HookhomeComponent implements OnInit, DoCheck, OnChanges {
 
   counter = 0;
   constructor() { }
+
+  // 因無 @Input 所以不會觸發 ngOnChanges
+  ngOnChanges(changes: SimpleChanges): void {
+    console.log("changes");
+  }
+
+  ngDoCheck(): void {
+    console.log("DoCheck");
+  }
 
   ngOnInit(): void {
   }
